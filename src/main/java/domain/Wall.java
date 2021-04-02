2626f9fbe7cb21b562d53d4101ae65fd20971243
Aigul Biktasheva <meleuz13@gmail.com>
2021-04-02 22:02:06 +0500

    initial commit

---
+++ src/main/java/domain/Wall.java
@@ -0,0 +1,229 @@
+package domain;
+
+public class Wall {
+    private int id; /*note id*/
+    private int ownerId; /*owner's id whose wall has the note*/
+    private int fromId; /*id of the person who left the note*/
+    private int createdBy; /*administrator's ID who published the note*/
+    private int replyOwnerId; /*user's ID whose note for was the current note published*/
+    private int replyPostId; /*note ID which is the parent of the current note*/
+    private boolean friendsOnly; /*true, if the note published only for friends*/
+    private int date; /*publishing date*/
+    private String text; /*note*/
+    private CommentsInfo commentsInfo; /*more info in the class*/
+    private Copyright copyright; /*more info in the class*/
+    private LikesInfo likesInfo; /*more info in the class*/
+    private Reposts reposts; /*more info in the class*/
+    private String postType; /*information ab the way the note was created: post, copy, reply, postpone, suggest*/
+    private Geo geo; /*more info in the class*/
+    private int views; /*number of views*/
+    private int signerId; /*author's ID in case if the note was published by the group*/
+    private boolean canPin; /*true, if the current user may fix the note on the page*/
+    private boolean canDelete; /*true, if the current user may delete the note*/
+    private boolean canEdit; /*true, if the current user may redact the note*/
+    private boolean isPinned; /*true, if the note was fixed*/
+    private boolean markedAsAds; /*true, if the note marked as advertising*/
+    private boolean isFavourite; /*true, if the note was added in favourites by the current user*/
+    private Donut donut; /*more info in the class*/
+    private int postponedId; /*Id of the note of it was on the timer*/
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public boolean isFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(boolean friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public Copyright getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(Copyright copyright) {
+        this.copyright = copyright;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public Reposts getReposts() {
+        return reposts;
+    }
+
+    public void setReposts(Reposts reposts) {
+        this.reposts = reposts;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public boolean isCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(boolean canPin) {
+        this.canPin = canPin;
+    }
+
+    public boolean isCanDelete(boolean b) {
+        return canDelete;
+    }
+
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public boolean isCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(boolean canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+
+    public boolean isMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(boolean markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavourite() {
+        return isFavourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        isFavourite = favourite;
+    }
+
+    public Donut getDonut() {
+        return donut;
+    }
+
+    public void setDonut(Donut donut) {
+        this.donut = donut;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
+    }
+}
