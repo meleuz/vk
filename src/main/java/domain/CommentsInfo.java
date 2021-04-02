0650e2ab302e4d86286a3a0f9f4332ed5dce80a7
Aigul Biktasheva <meleuz13@gmail.com>
2021-04-02 22:37:10 +0500

    Manager added

--- src/main/java/domain/CommentsInfo.java
+++ src/main/java/domain/CommentsInfo.java
@@ -2,21 +2,45 @@ package domain;
 
 public class CommentsInfo {
     private int count; /*number of comments*/
-    private boolean canPost; /*trues, if the current user may left a comment*/
+    private boolean canPost; /*true, if the current user may left a comment*/
+    private boolean groupsCanPost; /*true, if any group may left a comment*/
+    private boolean canClose; /*true, if the current user may block comments*/
+    private boolean canOpen; /*true, if the current user may unblock comments*/
 
     public int getCount() {
         return count;
     }
-
     public void setCount(int count) {
         this.count = count;
     }
-
     public boolean isCanPost() {
         return canPost;
     }
-
     public void setCanPost(boolean canPost) {
         this.canPost = canPost;
+    }
+
+    public boolean isGroupsCanPost() {
+        return groupsCanPost;
+    }
+
+    public void setGroupsCanPost(boolean groupsCanPost) {
+        this.groupsCanPost = groupsCanPost;
+    }
+
+    public boolean isCanClose() {
+        return canClose;
+    }
+
+    public void setCanClose(boolean canClose) {
+        this.canClose = canClose;
+    }
+
+    public boolean isCanOpen() {
+        return canOpen;
+    }
+
+    public void setCanOpen(boolean canOpen) {
+        this.canOpen = canOpen;
     }
 }
